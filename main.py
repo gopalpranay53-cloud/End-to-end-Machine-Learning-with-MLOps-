bfdcfd4449dd9c4908555519cb488d0b654a799f
Gopal Pranay <gopalpranay53@gmail.com>
2026-01-20 21:07:50 +0530

    model trainer added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 from MLOps_Project import logger
 from MLOps_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLOps_Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from MLOps_Project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from MLOps_Project.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME  = "Data Ingestion Stage"
 
@@ -29,3 +31,28 @@ if __name__ == '__main__':
         logger.exception(e)
         raise e
             
+            
+STAGE_NAME = "Data Transformation Stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e            
+    
+
+STAGE_NAME = "Model Trainer Stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e        
