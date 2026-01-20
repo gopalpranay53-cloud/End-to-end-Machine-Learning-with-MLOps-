5a8155923dabc2d81d063d39465a0773fbbadda1
Gopal Pranay <gopalpranay53@gmail.com>
2026-01-20 15:29:10 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from MLOps_Project import logger
 from MLOps_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from MLOps_Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME  = "Data Ingestion Stage"
 
@@ -13,3 +14,18 @@ if __name__ == '__main__':
     except Exception as e:
         logger.exception(e)
         raise e
+    
+
+
+
+STAGE_NAME = "Data Validation Stage"
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+            
